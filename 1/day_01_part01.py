0c9560bf5b9806bdd0a57b7d9628e335ec9e9c28
cnovel <github@kosmon.fr>
2020-12-02 13:49:35 +0100

    Optimize search

--- 1/day_01_part01.py
+++ 1/day_01_part01.py
@@ -3,9 +3,12 @@ import time
 
 def process():
     with open("input.txt", 'r') as f:
-        lines = [int(x.strip()) for x in f.readlines()]
+        lines = sorted([int(x.strip()) for x in f.readlines()])
         for i in range(0, len(lines)):
             for j in range(i+1, len(lines)):
+                # List is sorted, if number are higher than 2020, no need to continue
+                if lines[i]+lines[j] > 2020:
+                    break
                 if lines[i]+lines[j] == 2020:
                     print(lines[i]*lines[j])
                     return
@@ -14,4 +17,4 @@ def process():
 tic = time.perf_counter()
 process()
 toc = time.perf_counter()
-print(f"Found in {toc - tic:0.3f}s")
+print(f"Found in {toc - tic:0.4f}s")
